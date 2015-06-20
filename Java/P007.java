22b9075158f0dcfe527e07add8c0705bcff01832
Stefan M-F <StefanKMF@users.noreply.github.com>
2015-06-19 22:09:00 -0400

    Added solution to problem 7 in Java.

---
+++ Java/P007.java
@@ -0,0 +1,44 @@
+/* Project Euler problem 7 Solution
+ * by Stefan KMF
+ * StefanKristoff.ca
+*/
+
+public class P007{
+
+  public static void main(String[] args){
+    System.out.println(new P007().getPrime(10001));
+  }
+
+  public long getPrime(int num){
+    int[] oldPrimes;
+
+    oldPrimes = new int[num];
+    oldPrimes[0] = 2;
+
+    int primeCounter = 1;
+    int newPrime = 3; //Start at 3 since we know 2 is prime and already in list of Primes.
+
+    while (primeCounter < num){
+
+      if (isPrime(newPrime) == true){
+        primeCounter++;
+        oldPrimes[primeCounter-1] = newPrime;
+      }
+
+      newPrime += 2;
+    }
+
+  return oldPrimes[num-1];
+
+  }
+
+  public boolean isPrime(int num){
+    for (int x=2; x < Math.sqrt(num); x++){
+      if (num % x == 0)
+        return false;
+    }
+
+    return true;
+  }
+
+}
