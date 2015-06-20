78eb627875694a2b5d07982af157c8a9306e5cea
Stefan M-F <StefanKMF@users.noreply.github.com>
2015-06-19 20:42:03 -0400

    Added solution to problem 6 in Java.

---
+++ Java/P006.java
@@ -0,0 +1,25 @@
+/* Project Euler problem 5 Solution
+ * by Stefan KMF
+ * StefanKristoff.ca
+*/
+
+public class P006{
+
+  public static void main(String[] args){
+    System.out.println(new P006().squareDifference());
+  }
+
+  public long squareDifference(){
+    long sumSquares = 0;
+    long squareSum = 0;
+    for (long x=1; x <= 100; x++){
+      sumSquares += Math.pow(x,2l);
+      squareSum += x;
+    }
+
+    squareSum = squareSum*squareSum;
+
+    return (squareSum - sumSquares);
+  }
+
+}
